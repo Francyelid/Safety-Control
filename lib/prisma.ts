0bb26d1fbeea261d9162b727fcebd72bb17bd718
Francyelid <francyelid.lari@gmail.com>
2021-10-02 19:30:06 -0300

    declaração da variavel prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,6 +3,14 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: any;
+    }
+  }
+}
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
