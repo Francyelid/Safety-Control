0979fffa31512d4c4a45854348f5b999d69020ac
kevin.santos <kevinsantos12111@gmail.com>
2021-10-03 15:34:03 -0300

    # Criação da API - Adição de um repositório genérico e models

---
+++ pages/api/models/UserModel.ts
@@ -0,0 +1,15 @@
+class User {
+    id            :number;
+    name          :string;
+    email         :string;
+    password      :string;
+    type          :number;       
+    createdAt     :Date;
+    updatedAt     :Date;
+
+    constructor() {
+    }
+
+}
+
+export default User;
